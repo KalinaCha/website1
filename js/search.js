fc5133e9321912ad5f92f2009e812f768d51ac32
KalinaCha <kalinagaosheng@gmail.com>
2025-03-07 03:59:46 +0000

    Thu, Mar 6, 2025, 7:59 PM -08:00

--- js/search.js
+++ js/search.js
@@ -1,6 +1,8 @@
-btn = document.querySelector('.btn')
-search = document.querySelector('.search')
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
 
-btn.addEventListener('click',() = > {
-    search.classList.add('active')
+btn.addEventListener('click',() => {
+    search.classList.toggle('active')
+    input.focus()
 })
